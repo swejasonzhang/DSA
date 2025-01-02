2b41d548090dd6023ced4c1821a3cf4ed5ca7ddc
swejasonzhang <swejasonzhang@gmail.com>
2025-01-01 23:18:25 -0500

    finished hasDuplicate

--- Practice.py
+++ Practice.py
@@ -1,7 +1,5 @@
 # Palindromic Substrings
 
-# Medium On Neetcode
-
 # Given a string s, return the number of substrings within s that are palindromes.
 # A palindrome is a string that reads the same forward and backward.
 
@@ -37,4 +35,31 @@ class Solution:
                 res += (l >= r)
                 
         return res
+
+
+# Contains Duplicate
+
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+# Example 1:
+
+# Input: nums = [1, 2, 3, 3]
+# Output: true
+
+# Example 2:
+
+# Input: nums = [1, 2, 3, 4]
+# Output: false
+
+class Solution:
+    def hasDuplicate(self, nums: list) -> bool:
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+
+            seen.add(num)
+
+        return False
 
