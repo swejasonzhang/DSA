9642c2c19586e4fad1882e90194b8b65ef8ee601
swejasonzhang <swejasonzhang@gmail.com>
2025-01-02 23:29:55 -0500

    finished twoSum

--- Practice.py
+++ Practice.py
@@ -88,4 +88,38 @@ class Solution:
             countT[t[i]] = 1 + countT.get(t[i], 0)
         return countS == countT
 
-        
+# Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+# You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+# Return the answer with the smaller index first.
+
+# Example 1:
+
+# Input: 
+# nums = [3,4,5,6], target = 7
+
+# Output: [0,1]
+# Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+
+# Example 2:
+
+# Input: nums = [4,5,6], target = 10
+
+# Output: [0,2]
+# Example 3:
+
+# Input: nums = [5,5], target = 10
+
+# Output: [0,1]
+
+class Solution:
+    def twoSum(self, nums: list, target: int) -> list:
+        dict = {}
+
+        for index,num in enumerate(nums):
+            complement = target - num
+
+            if complement in dict:
+                return [dict[complement], index]
+
+            dict[num] = index       
