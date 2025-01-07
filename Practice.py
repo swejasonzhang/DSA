f2c4dac8b5cf834925f49541bc570013597b76f0
swejasonzhang <swejasonzhang@gmail.com>
2025-01-06 23:08:48 -0500

    finished reverseVowels problem

--- Practice.py
+++ Practice.py
@@ -194,3 +194,27 @@ class Solution(object):
 
         return count >= n
     
+# Given a string s, reverse only all the vowels in the string and return it.
+# The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        i, j = 0, len(s) - 1
+        vowels = "aeiouAEIOU"
+        s = list(s)
+
+        while i < j:
+            if s[i] in vowels and s[j] in vowels:
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+            elif s[i] not in vowels:
+                i += 1
+            else:
+                j -= 1
+
+        return "".join(s)
