ad8f3a069eb5c2fb7c93211670e2fbeccadaf518
swejasonzhang <swejasonzhang@gmail.com>
2025-01-07 23:03:39 -0500

    finished isSubsequence

--- Practice.py
+++ Practice.py
@@ -248,3 +248,38 @@ class Solution(object):
                     nums[i] = 0
                 non_zero_pos += 1
                 
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+# Example 1:
+
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+
+# Example 2:
+
+# Input: s = "axc", t = "ahbgdc"
+# Output: false
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        if len(s) == 0:
+            return True
+
+        i, j = 0, 0 
+
+        while j < len(t):
+            if s[i] == t[j]:
+                i += 1 
+            j += 1 
+            
+            if i == len(s):
+                return True
+
+        return i == len(s)
