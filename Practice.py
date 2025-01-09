b3d1102c30a60559c78b958fd842872748771f96
swejasonzhang <swejasonzhang@gmail.com>
2025-01-08 22:08:33 -0500

    finished largestAltitude

--- Practice.py
+++ Practice.py
@@ -315,3 +315,35 @@ class Solution(object):
             max_sum = max(max_sum, current_sum)
         
         return max_sum / float(k)
+
+# There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0
+# You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+# Example 1:
+
+# Input: gain = [-5,1,5,0,-7]
+# Output: 1
+# Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+ 
+#Example 2:
+
+# Input: gain = [-4,-3,-2,-1,4,3,2]
+# Output: 0
+# Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+
+class Solution(object):
+    def largestAltitude(self, gain):
+        """
+        :type gain: List[int]
+        :rtype: int
+        """
+        
+        res = 0
+        total = 0
+        
+        for i in range(len(gain)):
+            total += gain[i]
+
+            res = max(total, res)
+
+        return res
