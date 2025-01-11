1c644f9f79ee4b2abc16630b9dc6ef7c199888d9
swejasonzhang <swejasonzhang@gmail.com>
2025-01-10 20:27:54 -0500

    finished searchBST

--- Practice.py
+++ Practice.py
@@ -599,3 +599,38 @@ class Solution(object):
             return False
 
         return self.traverseTree(root1) == self.traverseTree(root2)
+
+# You are given the root of a binary search tree (BST) and an integer val.
+# Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null. 
+
+# Example 1:
+
+# Input: root = [4,2,7,1,3], val = 2
+# utput: [2,1,3]
+
+# Example 2:
+
+# Input: root = [4,2,7,1,3], val = 5
+# Output: []
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        
+        if root is None or root.val == val:
+            return root
+
+        if val < root.val:
+            return self.searchBST(root.left, val)
+
+        return self.searchBST(root.right, val)
