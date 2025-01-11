4ae8add710347952207723937e1e21dfaa6f10b6
swejasonzhang <swejasonzhang@gmail.com>
2025-01-10 20:15:19 -0500

    finished leafSimilar

--- Practice.py
+++ Practice.py
@@ -552,3 +552,50 @@ class Solution(object):
 
         return max(left, right) + 1
     
+# Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+# For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+# wo binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+# Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+# Example 1:
+
+# Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+# Output: true
+
+# Example 2:
+
+# Input: root1 = [1,2,3], root2 = [1,3,2]
+# Output: false
+
+ # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def traverseTree(self, root):
+        if root is None:
+            return []
+
+        if root.left is None and root.right is None:
+            return [root.val]
+    
+        left = self.traverseTree(root.left)
+        right = self.traverseTree(root.right)
+
+        return left + right
+
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: Optional[TreeNode]
+        :type root2: Optional[TreeNode]
+        :rtype: bool
+        """
+        if root1 is None or root2 is None:
+            return False
+
+        return self.traverseTree(root1) == self.traverseTree(root2)
