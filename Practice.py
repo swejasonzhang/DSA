8df0bcd8d30908253e375ad6ca4c941de4d8aa48
swejasonzhang <swejasonzhang@gmail.com>
2025-01-10 19:30:02 -0500

    finished unique occurences

--- Practice.py
+++ Practice.py
@@ -430,3 +430,45 @@ class Solution(object):
         difference2 = set(nums2) - set(nums1)
 
         return [list(difference1), list(difference2)]
+    
+# Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+# Example 1:
+
+# Input: arr = [1,2,2,1,1,3]
+# Output: true
+# Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+# Example 2:
+
+# Input: arr = [1,2]
+# Output: false
+# Example 3:
+
+# Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+# Output: true
+
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+
+        dict = {}
+        
+        for num in arr:
+            if num not in dict:
+                dict[num] = 0
+            dict[num] += 1
+
+        res = set()
+
+        for keys in dict:
+            if dict[keys] in res:
+                return False
+            
+            res.add(dict[keys])
+
+        return True
+
