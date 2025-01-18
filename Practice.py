e99832d6f2eff93afce02c2644bed62612f22bf1
swejasonzhang <swejasonzhang@gmail.com>
2025-01-18 00:35:14 -0500

    finished findDisappearedNumbers

--- Practice.py
+++ Practice.py
@@ -834,3 +834,26 @@ class Solution(object):
             i += 1
 
         return result
+    
+# Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+# Example 1:
+
+# Input: nums = [4,3,2,7,8,2,3,1]
+# Output: [5,6]
+# Example 2:
+
+# Input: nums = [1,1]
+# Output: [2]
+ 
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        full_set = set(range(1, len(nums) + 1))
+        nums_set = set(nums)
+        
+        return list(full_set - nums_set)
