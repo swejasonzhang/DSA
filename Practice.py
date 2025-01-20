86b4e32ac2c9b603e12ba1bcb4ef8847564fb7c8
swejasonzhang <swejasonzhang@gmail.com>
2025-01-19 23:05:46 -0500

    completed isArraySpecial

--- Practice.py
+++ Practice.py
@@ -857,3 +857,55 @@ class Solution(object):
         nums_set = set(nums)
         
         return list(full_set - nums_set)
+    
+# An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
+# You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
+
+# Example 1:
+
+# Input: nums = [1]
+# Output: true
+
+# Explanation:
+
+# There is only one element. So the answer is true.
+
+# Example 2:
+
+# Input: nums = [2,1,4]
+# Output: true
+
+# Explanation:
+
+# There is only two pairs: (2,1) and (1,4), and both of them contain numbers with different parity. So the answer is true.
+
+# Example 3:
+
+# Input: nums = [4,3,1,6]
+# Output: false
+
+# Explanation:
+# nums[1] and nums[2] are both odd. So the answer is false.
+
+class Solution(object):
+    def isArraySpecial(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        if len(nums) == 1:
+            return True
+
+        i, j = 0, 1
+
+        while j < len(nums):
+            if nums[i] % 2 == 0 and nums[j] % 2 == 0:
+                return False
+            elif nums[i] % 2 == 1 and nums[j] % 2 == 1:
+                return False
+
+            i += 1
+            j += 1
+
+        return True
