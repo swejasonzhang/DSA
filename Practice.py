4c7560cb91ed3878854579fab024b2532a5d8ae9
swejasonzhang <swejasonzhang@gmail.com>
2025-01-20 23:52:08 -0500

    completed strStr

--- Practice.py
+++ Practice.py
@@ -909,3 +909,39 @@ class Solution(object):
             j += 1
 
         return True
+
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+# Example 1:
+
+# Input: haystack = "sadbutsad", needle = "sad"
+# Output: 0
+# Explanation: "sad" occurs at index 0 and 6.
+# The first occurrence is at index 0, so we return 0.
+
+# Example 2:
+
+# Input: haystack = "leetcode", needle = "leeto"
+# Output: -1
+# Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        
+        i, j = 0, len(needle) - 1 
+
+        while j < len(haystack):
+            substring = haystack[i:j + 1]
+
+            if substring == needle:
+                return i
+
+            i += 1
+            j += 1
+
+        return -1 
