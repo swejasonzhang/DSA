528819c573a6b06abf9b8c3b288c125903330136
swejasonzhang <swejasonzhang@gmail.com>
2025-01-24 21:14:09 -0500

    finished minimumOperations

--- Practice.py
+++ Practice.py
@@ -1182,3 +1182,39 @@ class Solution(object):
                 result.append(index)
 
         return result
+    
+# You are given an integer array nums. In one operation, you can add or subtract 1 from any element of nums.
+# Return the minimum number of operations to make all elements of nums divisible by 3.
+
+# Example 1:
+# Input: nums = [1,2,3,4]
+
+# Output: 3
+
+# Explanation:
+
+# All array elements can be made divisible by 3 using 3 operations:
+
+# Subtract 1 from 1.
+# Add 1 to 2.
+# Subtract 1 from 4.
+
+# Example 2:
+# Input: nums = [3,6,9]
+
+# Output: 0
+
+class Solution(object):
+    def minimumOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        counter = 0
+        
+        for num in nums:
+            if (num + 1) % 3 == 0 or (num - 1) % 3 == 0:
+                counter += 1
+
+        return counter
