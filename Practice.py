b83ea0de1a63d6f9478cc1b5338180bd419166fe
swejasonzhang <swejasonzhang@gmail.com>
2025-01-24 21:06:35 -0500

    finished fishWordContaining

--- Practice.py
+++ Practice.py
@@ -1144,3 +1144,41 @@ class Solution(object):
             return True
 
         return False
+
+# You are given a 0-indexed array of strings words and a character x.
+# Return an array of indices representing the words that contain the character x.
+# Note that the returned array may be in any order.
+
+# Example 1:
+
+# nput: words = ["leet","code"], x = "e"
+# Output: [0,1]
+# Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+
+# Example 2:
+
+# Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+# Output: [0,2]
+# Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+
+# Example 3:
+
+# Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+# Output: []
+# Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        """
+        :type words: List[str]
+        :type x: str
+        :rtype: List[int]
+        """
+
+        result = []
+        
+        for index, word in enumerate(words):
+            if x in word:
+                result.append(index)
+
+        return result
