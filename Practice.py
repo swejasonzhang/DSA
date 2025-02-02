0c36bb1e1fcc95c4b39a077ffc89d577449d37b2
swejasonzhang <swejasonzhang@gmail.com>
2025-02-01 20:41:13 -0500

    finished greatestLetter

--- Practice.py
+++ Practice.py
@@ -1635,3 +1635,41 @@ class Solution(object):
         count_second_half = sum(1 for char in s[mid:] if char in vowels)
         
         return count_first_half == count_second_half
+    
+# Given a string of English letters s, return the greatest English letter which occurs as both a lowercase and uppercase letter in s. The returned letter should be in uppercase. If no such letter exists, return an empty string.
+# An English letter b is greater than another letter a if b appears after a in the English alphabet.
+
+# Example 1:
+
+# Input: s = "lEeTcOdE"
+# Output: "E"
+# Explanation:
+# The letter 'E' is the only letter to appear in both lower and upper case.
+# Example 2:
+
+# Input: s = "arRAzFif"
+# Output: "R"
+# Explanation:
+# The letter 'R' is the greatest letter to appear in both lower and upper case.
+# Note that 'A' and 'F' also appear in both lower and upper case, but 'R' is greater than 'F' or 'A'.
+
+# Example 3:
+# Input: s = "AbCdEfGhIjK"
+# Output: ""
+# Explanation:
+# There is no letter that appears in both lower and upper case.
+
+class Solution(object):
+    def greatestLetter(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        unique_chars = set(s)
+        greatest = ""
+
+        for char in unique_chars:
+            if char.isupper() and char.lower() in unique_chars:
+                greatest = max(greatest, char)
+
+        return greatest
