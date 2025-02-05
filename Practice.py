9279508c273b67a97aa78fe3eccc7fb0bb906363
swejasonzhang <swejasonzhang@gmail.com>
2025-02-04 21:44:12 -0500

    finished wordPattern

--- Practice.py
+++ Practice.py
@@ -1673,3 +1673,56 @@ class Solution(object):
                 greatest = max(greatest, char)
 
         return greatest
+
+# Given a pattern and a string s, find if s follows the same pattern.
+# Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s. Specifically:
+# Each letter in pattern maps to exactly one unique word in s.
+# Each unique word in s maps to exactly one letter in pattern.
+# No two letters map to the same word, and no two words map to the same letter.
+
+# Example 1:
+
+# Input: pattern = "abba", s = "dog cat cat dog"
+# Output: true
+
+# Explanation:
+# The bijection can be established as:
+# 'a' maps to "dog".
+# 'b' maps to "cat".
+
+# Example 2:
+
+# Input: pattern = "abba", s = "dog cat cat fish"
+# Output: false
+
+# Example 3:
+# Input: pattern = "aaaa", s = "dog cat cat dog"
+# Output: false
+
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        
+        char_to_word = {}
+        word_to_char = {}
+
+        for i in range(len(pattern)):
+            char = pattern[i]
+            word = words[i]
+
+            if char in char_to_word and char_to_word[char] != word:
+                return False
+            if word in word_to_char and word_to_char[word] != char:
+                return False
+
+            char_to_word[char] = word
+            word_to_char[word] = char
+
+        return True
