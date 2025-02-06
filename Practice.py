683fd38ef07286a3df610ec55b91d843a691fbc7
swejasonzhang <swejasonzhang@gmail.com>
2025-02-05 20:50:27 -0500

    finished longestMountain

--- Practice.py
+++ Practice.py
@@ -1726,3 +1726,54 @@ class Solution(object):
             word_to_char[word] = char
 
         return True
+    
+# You may recall that an array arr is a mountain array if and only if:
+
+# arr.length >= 3
+# There exists some index i (0-indexed) with 0 < i < arr.length - 1 such that:
+# arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+# arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+# Given an integer array arr, return the length of the longest subarray, which is a mountain. Return 0 if there is no mountain subarray.
+
+# Example 1:
+
+# Input: arr = [2,1,4,7,3,2,5]
+# Output: 5
+# Explanation: The largest mountain is [1,4,7,3,2] which has length 5.
+
+# Example 2:
+
+# Input: arr = [2,2,2]
+# Output: 0
+# Explanation: There is no mountain.
+
+class Solution(object):
+    def longestMountain(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+
+        n = len(arr)
+        if n < 3:
+            return 0 
+
+        longest = 0
+        i = 1 
+
+        while i < n - 1:
+            if arr[i - 1] < arr[i] > arr[i + 1]:
+                left = i - 1
+                while left > 0 and arr[left - 1] < arr[left]:
+                    left -= 1 
+
+                right = i + 1
+                while right < n - 1 and arr[right] > arr[right + 1]:
+                    right += 1 
+
+                longest = max(longest, right - left + 1)
+                i = right 
+            else:
+                i += 1
+
+        return longest
