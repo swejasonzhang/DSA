a2a8ea2b9575f4ccf87055d33983f813f7649bdc
swejasonzhang <swejasonzhang@gmail.com>
2025-02-06 21:38:46 -0500

    finished singleNumber

--- Practice.py
+++ Practice.py
@@ -1777,3 +1777,43 @@ class Solution(object):
                 i += 1
 
         return longest
+
+# Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+# You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+
+# Example 1:
+
+# Input: nums = [1,2,1,3,2,5]
+# Output: [3,5]
+# Explanation:  [5, 3] is also a valid answer.
+
+# Example 2:
+
+# Input: nums = [-1,0]
+# Output: [-1,0]
+
+# Example 3:
+
+# Input: nums = [0,1]
+# Output: [1,0]
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()  
+        result = []
+
+        i = 0
+        while i < len(nums):
+            if (i == len(nums) - 1) or (nums[i] != nums[i + 1]):
+                result.append(nums[i])
+                if len(result) == 2:
+                    return result
+                i += 1
+            else:
+                i += 2 
+
+        return result
