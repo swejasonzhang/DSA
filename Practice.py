ec53d6ef55364f44da0a901d72d1bed6c1fde52b
swejasonzhang <swejasonzhang@gmail.com>
2025-02-20 22:32:43 -0500

    finished sortArrayByParity2

--- Practice.py
+++ Practice.py
@@ -1958,3 +1958,34 @@ class Solution(object):
                 max_val = max(max_val, len(s))
                 
         return max_val
+    
+# Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+# Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+# Return any answer array that satisfies this condition.
+
+# Example 1:
+
+# Input: nums = [4,2,5,7]
+# Output: [4,5,2,7]
+# Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+
+# Example 2:
+
+# Input: nums = [2,3]
+# Output: [2,3]
+
+def sortArrayByParityII(nums):
+    n = len(nums)
+    i, j = 0, 1 
+    
+    while i < n and j < n:
+        if nums[i] % 2 == 0: 
+            i += 2
+        elif nums[j] % 2 == 1: 
+            j += 2
+        else:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 2
+            j += 2
+            
+    return nums
