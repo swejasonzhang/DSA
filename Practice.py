7eb584fc5b2e85ee6554002befaaabd8575195ea
swejasonzhang <swejasonzhang@gmail.com>
2025-03-07 00:34:32 -0500

    finished minimumOperations

--- Practice.py
+++ Practice.py
@@ -2079,3 +2079,42 @@ def wordPattern(pattern: str, s: str) -> bool:
             word_to_char[word] = char
 
     return True
+
+# You are given an integer array nums. Each element in nums is 1, 2 or 3. In each operation, you can remove an element from nums. Return the minimum number of operations to make nums non-decreasing.
+
+# Example 1:
+# Input: nums = [2,1,3,2,1]
+# Output: 3
+
+# Explanation:
+# One of the optimal solutions is to remove nums[0], nums[2] and nums[3].
+
+# Example 2:
+# Input: nums = [1,3,2,1,3,3]
+# Output: 2
+
+# Explanation:
+# One of the optimal solutions is to remove nums[1] and nums[2].
+
+# Example 3:
+# Input: nums = [2,2,2,2,3,3]
+# Output: 0
+
+# Explanation:
+# nums is already non-decreasing.
+
+class Solution(object):
+    def minimumOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a, b, c = 0, 0, 0
+
+        for num in nums:
+            a += num != 1 
+            b = min(a, b + (num != 2))
+            c = min(b, c + (num != 3))
+            
+        return c
+    
