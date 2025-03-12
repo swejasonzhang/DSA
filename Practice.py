8265247e91c4e4d9731de0778c0992ecc1818c45
swejasonzhang <swejasonzhang@gmail.com>
2025-03-12 00:01:05 -0400

    finished isPowerOfFour

--- Practice.py
+++ Practice.py
@@ -2118,3 +2118,31 @@ class Solution(object):
             
         return c
     
+# Given an integer n, return true if it is a power of four. Otherwise, return false.
+# An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+# Example 1:
+# Input: n = 16
+# Output: true
+
+# Example 2:
+# Input: n = 5
+# tput: false
+
+# Example 3:
+# Input: n = 1
+# Output: true
+
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        
+        while n % 4 == 0:
+            n //= 4
+        
+        return n == 1
