334d4dd8b53449ddba7b25d8e76f65c655a6fe65
swejasonzhang <swejasonzhang@gmail.com>
2025-03-14 00:04:20 -0400

    finished smallestNumber

--- Practice.py
+++ Practice.py
@@ -2146,3 +2146,37 @@ class Solution(object):
             n //= 4
         
         return n == 1
+    
+# You are given two integers n and t. Return the smallest number greater than or equal to n such that the product of its digits is divisible by t.
+
+# Example 1:
+# Input: n = 10, t = 2
+# Output: 10
+
+# Explanation:
+# The digit product of 10 is 0, which is divisible by 2, making it the smallest number greater than or equal to 10 that satisfies the condition.
+
+# Example 2:
+# Input: n = 15, t = 3
+# Output: 16
+
+# Explanation:
+# The digit product of 16 is 6, which is divisible by 3, making it the smallest number greater than or equal to 15 that satisfies the condition.
+
+class Solution(object):
+    def smallestNumber(self, n, t):
+        """
+        :type n: int
+        :type t: int
+        :rtype: int
+        """
+        num = n
+        while True:
+            string = str(num)
+            if len(string) == 1: 
+                if int(string) % t == 0:
+                    return num
+            else:
+                if (int(string[0]) * int(string[1])) % t == 0:
+                    return num
+            num += 1
